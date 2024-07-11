385dd062469b74b7bf04e4b9f8594df3caa864c4
Sara López Cardona <sara.lopez@globalmvm.com>
2024-07-11 11:01:31 -0500

    Agenda visualmente finalizada

---
+++ agenda/script.js
@@ -0,0 +1,16 @@
+events: [
+    {
+      title: 'Evento 1',
+      start: '2024-07-10'
+    },
+    {
+      title: 'Evento 2',
+      start: '2024-07-11',
+      end: '2024-07-13'
+    },
+    {
+      title: 'Evento 3',
+      start: '2024-07-15T10:00:00',
+      end: '2024-07-15T12:00:00'
+    }
+  ]
